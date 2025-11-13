5707fc1d62060cc798bd8dfcc54c679cf83cdc40
Sebastian Cortes <sebastiancortes303190@correo.itm.edu.co>
2025-11-13 05:51:48 -0500

    Add CategoryRepository and ICategoryRepository
    Removed the `Repository\IRepository` folder from the project file to simplify the folder structure. Added `CategoryRespository` class implementing `ICategoryRepository` for category management using Entity Framework Core. Introduced methods for checking existence, creating, updating, and deleting categories. Utilized dependency injection for `ApplicationDbContext` and implemented asynchronous operations for database interactions.

---
+++ API.J.Movies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.J.Movies.DAL.Models;
+
+namespace API.J.Movies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); //Me retorna una lista de categorias
+        Task<Category> GetCategoryByIdAsync(int id); //Me retorna Una categoria por ID
+        Task<bool> CategoryExistsByIdAsync(int id); //Me dice si existe una categoria por ID
+        Task<bool> CategoryExistsByNameAsync(string name); //Me dice si existe una categoria por Nombre
+        Task<bool> CreateCategoryAsync(Category category); //Crea una categoria
+        Task<bool> UpdateCategoryAsync(Category category); // Actualiza una categoria --nombre y fecha
+        Task<bool> DeleteCategoryAsync(int id); //Me elimina una categoria
+    }
+}
