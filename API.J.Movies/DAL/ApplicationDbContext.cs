38d34b996a33e0ad8c48f9c50041a593ec729730
Sebastian Cortes <sebastiancortes303190@correo.itm.edu.co>
2025-11-13 04:33:44 -0500

    Add project files.

---
+++ API.J.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.J.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.J.Movies.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //Seccion para crear el dbset de las entidades o modelos
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ API.J.Movies/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.J.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] // Ensures that the Name property must have a value
+        [Display(Name = "Nombre de la categoria")] // Me sirve para poner un nombre amigable en las validaciones
+        public string Name { get; set; }
+    }
+}
